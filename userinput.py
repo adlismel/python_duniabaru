4afa7d5797a099b30ab158ca66b23f7369705bdd
Tok Munsyi <65755824+adlismel@users.noreply.github.com>
2020-05-23 13:55:27 +0800

    User input data

---
+++ userinput.py
@@ -0,0 +1,9 @@
+# Program untuk user masukkan ipput
+
+print("|--------------------------------------|")
+print("| SELAMAT DATANG KE PROGRAM BERKENALAN |")
+print("|--------------------------------------|")
+
+nama = input("Masukkan nama penuh anda : ")
+
+print("Hai ", nama, " selamat datang ke sistem kami")
