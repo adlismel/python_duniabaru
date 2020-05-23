f7b8ce7ca1b99e8fdab13bd5cb4c18066e17708e
Tok Munsyi <65755824+adlismel@users.noreply.github.com>
2020-05-23 13:56:18 +0800

    Update userinput.py

--- userinput.py
+++ userinput.py
@@ -5,5 +5,4 @@ print("| SELAMAT DATANG KE PROGRAM BERKENALAN |")
 print("|--------------------------------------|")
 
 nama = input("Masukkan nama penuh anda : ")
-
 print("Hai ", nama, " selamat datang ke sistem kami")
